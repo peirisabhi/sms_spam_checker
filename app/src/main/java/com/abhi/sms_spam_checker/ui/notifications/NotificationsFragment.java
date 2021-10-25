278dca586d45e0e50b1aef84b75760d1373f1e6f
abhishek dananjaya <abhishekdananjaya1@yahoo.com>
2021-10-25 16:26:12 +0530

    logout created

--- app/src/main/java/com/abhi/sms_spam_checker/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/abhi/sms_spam_checker/ui/notifications/NotificationsFragment.java
@@ -3,6 +3,7 @@ package com.abhi.sms_spam_checker.ui.notifications;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,8 +19,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.abhi.sms_spam_checker.AuthenticateActivity;
 import com.abhi.sms_spam_checker.R;
 import com.abhi.sms_spam_checker.databinding.FragmentNotificationsBinding;
+import com.abhi.sms_spam_checker.db.UserStore;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -32,6 +35,8 @@ public class NotificationsFragment extends Fragment {
 
     public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
     private final static String default_notification_channel_id = "default";
+
+    UserStore userStore;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -57,35 +62,55 @@ public class NotificationsFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.textView2.setOnClickListener(new View.OnClickListener() {
+        if (userStore == null) {
+            userStore = new UserStore(getActivity());
+        }
+
+//        binding.textView2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                System.out.println("clicked");
+//
+//                try {
+//                    NotificationManager notificationManager;
+//                    notificationManager = (NotificationManager) requireActivity().getSystemService(NOTIFICATION_SERVICE );
+//                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(requireContext(), default_notification_channel_id ) ;
+//                    mBuilder.setContentTitle("New Appointement");
+//                    mBuilder.setContentText("Test");
+//                    mBuilder.setSmallIcon(R.mipmap.ic_launcher) ;
+//                    mBuilder.setAutoCancel( true ) ;
+//                    if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+//                        int importance = NotificationManager. IMPORTANCE_HIGH ;
+//                        NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
+//                        mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
+//                        assert notificationManager != null;
+//                        notificationManager.createNotificationChannel(notificationChannel) ;
+//                    }
+//                    assert notificationManager != null;
+//                    notificationManager.notify(( int ) System. currentTimeMillis () , mBuilder.build()) ;
+//
+//
+//                } catch (Exception e) {
+//                    Log.e("TAG", "onReceive error: " + e.getLocalizedMessage());
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+
+
+        binding.box4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                System.out.println("clicked");
+                userStore.open();
+                userStore.drop();
+                userStore.close();
 
-                try {
-                    NotificationManager notificationManager;
-                    notificationManager = (NotificationManager) requireActivity().getSystemService(NOTIFICATION_SERVICE );
-                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(requireContext(), default_notification_channel_id ) ;
-                    mBuilder.setContentTitle("New Appointement");
-                    mBuilder.setContentText("Test");
-                    mBuilder.setSmallIcon(R.mipmap.ic_launcher) ;
-                    mBuilder.setAutoCancel( true ) ;
-                    if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
-                        int importance = NotificationManager. IMPORTANCE_HIGH ;
-                        NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
-                        mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
-                        assert notificationManager != null;
-                        notificationManager.createNotificationChannel(notificationChannel) ;
-                    }
-                    assert notificationManager != null;
-                    notificationManager.notify(( int ) System. currentTimeMillis () , mBuilder.build()) ;
+                Intent intent = new Intent(requireActivity(), AuthenticateActivity.class);
+                startActivity(intent);
+                getActivity().finish();
 
-
-                } catch (Exception e) {
-                    Log.e("TAG", "onReceive error: " + e.getLocalizedMessage());
-                    e.printStackTrace();
-                }
             }
         });
 
